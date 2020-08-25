f3fc5cbbccbac7f9a45c3a99283a23bc1f32a558
Manfred Steyer <manfred.steyer@gmx.net>
2020-08-25 16:59:44 +0200

    ddd

--- libs/luggage/feature-check-in/src/lib/luggage-feature-check-in.module.ts
+++ libs/luggage/feature-check-in/src/lib/luggage-feature-check-in.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { CheckInComponent } from './check-in.component';
+import { LuggageUiCardModule  } from '@flight-workspace/luggage/ui-card';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule],
+  imports: [CommonModule, LuggageDomainModule, LuggageUiCardModule],
   declarations: [CheckInComponent],
   exports: [CheckInComponent],
 })
--- libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
+++ libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { ReportLossComponent } from './report-loss.component';
+import { FlightApiModule } from '@flight-workspace/flight-api';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule],
+  imports: [CommonModule, LuggageDomainModule, FlightApiModule],
   declarations: [ReportLossComponent],
   exports: [ReportLossComponent],
 })
