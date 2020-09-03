7b55ac171e0ea24d85263c62e5e7e92c6b6044c8
Michael Zikes <michael.zikes@intauria.com>
2020-09-03 21:43:58 +0200

    local component state: migration

--- apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,12 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {FlightService, Flight} from '@flight-workspace/flight-api';
-import { FlightBookingAppState } from '../+state/flight-booking.reducer';
-import { Store, select } from '@ngrx/store';
-import { Observable, timer } from 'rxjs';
-import * as fromFlightBooking from '../+state/flight-booking.actions';
-import { take, tap, map } from 'rxjs/operators';
-import { selectFlights } from '../+state/flight-booking.selectors';
-import { FlightSearchStore } from './flight-search.store';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConnectableObservable, timer } from 'rxjs';
+import { publishReplay, map, take } from 'rxjs/operators';
+
+import { FlightSearchStore, FlightSearchFilter } from './flight-search.store';
 
 @Component({
   selector: 'flight-search',
@@ -14,28 +11,29 @@ import { FlightSearchStore } from './flight-search.store';
   styleUrls: ['./flight-search.component.css'],
   providers: [ FlightSearchStore ]
 })
-export class FlightSearchComponent implements OnInit {
-
-  from = 'Hamburg'; // in Germany
-  to = 'Graz'; // in Austria
-  urgent = false;
-
-  /* get flights() {
-    return this.flightService.flights;
-  } */
-
-  flights$: Observable<Flight[]>;
-
-  // "shopping basket" with selected flights
-  basket: object = {
-    "3": true,
-    "5": true
-  };
+export class FlightSearchComponent {
+  searchForm: FormGroup;
+  formChanges$: ConnectableObservable<FlightSearchFilter>;
 
   constructor(
-    private flightService: FlightService,
-    private store: Store<FlightBookingAppState>,
-    public componentStore: FlightSearchStore) {
+    public componentStore: FlightSearchStore,
+    private fb: FormBuilder) {
+
+    this.searchForm = this.fb.group({
+      from: [null, [Validators.required]],
+      to: [null, [Validators.required]],
+      urgent: []
+    });
+
+    this.formChanges$ = this.searchForm.valueChanges
+      .pipe(
+        publishReplay(1)
+      ) as ConnectableObservable<FlightSearchFilter>;
+    this.formChanges$.connect();
+
+    this.componentStore.updateForm(
+      this.searchForm.patchValue.bind(this.searchForm)
+    );
 
     // Connecting Observables directly to Updaters results in parallel updates.
     // The second Updater call with a new Observable does not complete the first Observable.
@@ -47,43 +45,10 @@ export class FlightSearchComponent implements OnInit {
     );
   }
 
-  ngOnInit() {
-    this.flights$ = this.store.pipe(
-      select(selectFlights)
-    );
-  }
-
   search(): void {
-    if (!this.from || !this.to) return;
-
-    /* this.flightService
-      .load(this.from, this.to, this.urgent); */
-
-    /* this.flightService
-      .find(this.from, this.to)
-      .subscribe(
-        flights => this.store.dispatch(fromFlightBooking.flightsLoaded({ flights }))
-      ); */
-
-    this.store.dispatch(fromFlightBooking.flightsLoad({ from: this.from, to: this.to }));
-  }
-
-  delay(): void {
-    //this.flightService.delay();
-
-    this.flights$.pipe(
-      take(1)
-    ).subscribe(
-      flights => {
-        const flight = flights[0];
-
-        const oldDate = new Date(flight.date);
-        const newDate = new Date(oldDate.getTime() + 1000 * 60 * 15);
-        const newFlight = { ...flight, date: newDate.toISOString() };
-
-        this.store.dispatch(fromFlightBooking.flightUpdate({ flight: newFlight }));
-      }
+    // Trigger and Effect and an Updater
+    this.componentStore.setFilter(
+      this.formChanges$.pipe(take(1))
     );
   }
-
 }
--- apps/flight-app/src/app/flight-booking/flight-search/flight-search.store.ts
+++ apps/flight-app/src/app/flight-booking/flight-search/flight-search.store.ts
@@ -13,11 +13,21 @@ import * as fromFlightBooking from '../+state';
 
 
 
+export interface FlightSearchFilter {
+  from: string;
+  to: string;
+  urgent?: boolean;
+}
+
 export interface FlightSearchState {
+  filter: FlightSearchFilter;
+  basket: { [key: string]: boolean };
   counter: number;
 }
 
 const initalState = {
+  filter: { from: 'Hamburg', to: 'Graz' },
+  basket: { '3': true, '5': true },
   counter: 0
 };
 
@@ -30,20 +40,62 @@ export class FlightSearchStore extends ComponentStore<FlightSearchState> {
    * Selectors
    ***************/
 
+  // Local State Selector
+  readonly filter$ = this.select(
+    state => state.filter,
+    { debounce: true }
+  );
+
+  // Global State Selector with Local Filter
+  private readonly flights$ = this.select(
+    this.store.select(fromFlightBooking.selectFlights),
+    this.filter$,
+    (flights, searchFilter) =>
+      flights.filter(f =>
+        f.from === searchFilter.from && f.to === searchFilter.to),
+    { debounce: true }
+  );
+
+  private readonly firstFlight$ = this.select(
+    this.flights$,
+    flights => flights[0],
+    { debounce: true }
+  );
+
+  private readonly basket$ = this.select(
+    state => state.basket
+  );
+
   private readonly counter$ = this.select(
     state => state.counter
-  )
+  );
 
   // Custom State slice to be used in the Template
   readonly componentState$ = this.select(
+    this.flights$,
+    this.basket$,
     this.counter$,
-    (counter) => ({ counter })
-  )
+    (flights, basket, counter) => ({ flights, basket, counter })
+  );
 
 
   /***************
    * Updaters
    ***************/
+
+  readonly updateFilter = this.updater(
+    (state, searchFilter: FlightSearchFilter) => ({
+      ...state,
+      filter: searchFilter
+    })
+  );
+
+  readonly updateBasket = this.updater(
+    (state, basket: { id: string, selected: boolean }) => ({
+      ...state,
+      basket: { ...state.basket, [basket.id]: basket.selected }
+    })
+  );
 
   readonly updateCounter = this.updater(
     (state, i: number) => ({
@@ -56,6 +108,85 @@ export class FlightSearchStore extends ComponentStore<FlightSearchState> {
   /***************
    * Effects
    ***************/
+
+  // Generic Flight delay Effect that dispatches a Global State Management Action
+  private readonly delayFlight = this.effect(
+    (payload$: Observable<{ flight: Flight, addTimeMin: number }>) =>
+      payload$.pipe(
+        tap((payload: { flight: Flight, addTimeMin: number }) =>
+          this.store.dispatch(
+            fromFlightBooking.flightUpdate({
+              flight: produce(payload.flight, flight => {
+                const date = new Date(flight.date).getTime() + 1000 * 60 * payload.addTimeMin;
+                flight.date = new Date(date).toISOString();
+              })
+            })
+          )
+        )
+      )
+  );
+
+  // Specific Flight delay Effect
+  readonly delayFirstFlightBy15Min = this.effect(
+    (trigger$: Observable<void>) =>
+      trigger$.pipe(
+        switchMap(_ => this.firstFlight$.pipe(take(1))),
+        tap((flight: Flight) =>
+          this.delayFlight({ flight, addTimeMin: 15 })
+        )
+      )
+  );
+
+  // Effect loads new Flights via Global State Management
+  private readonly loadflights = this.effect(
+    (filter$: Observable<FlightSearchFilter>) =>
+      filter$.pipe(
+        tap((searchFilter: FlightSearchFilter) =>
+          this.store.dispatch(
+            fromFlightBooking.flightsLoad({
+              from: searchFilter.from,
+              to: searchFilter.to,
+              urgent: searchFilter.urgent
+            })
+          )
+        )
+      )
+  );
+
+  // Trigger an Effect and an Updater
+  readonly setFilter = this.effect(
+    (filter$: Observable<FlightSearchFilter>) =>
+      filter$.pipe(
+        tap((searchFilter: FlightSearchFilter) =>
+          this.loadflights(searchFilter)
+        ),
+        tap((searchFilter: FlightSearchFilter) =>
+          this.updateFilter(searchFilter)
+        )
+      )
+  );
+
+  // Update RX Forms based on a Local State change via Observable Selector w/o direct Subscription
+  readonly updateForm = this.effect(
+    (patchValueFn$: Observable<typeof FormGroup.prototype.patchValue>) =>
+      patchValueFn$.pipe(
+        switchMap(patchValueFn =>
+          this.filter$.pipe(
+            tap(searchFilter =>
+              patchValueFn(searchFilter)
+            )
+          )
+        )
+      )
+  );
+
+  // Factory that returns an Effect to automatically update the Local Filter State once, based on the current RX Forms State
+  readonly updateFilterByForm = (searchFilter$: Observable<FlightSearchFilter>) => this.effect(
+    ($trigger: Observable<FlightSearchFilter>) =>
+      $trigger.pipe(
+        map(searchFilter => this.updateFilter(searchFilter$.pipe(take(1))))
+      )
+  );
 
 
 
